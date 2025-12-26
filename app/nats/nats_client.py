f0d2220156d2e2232e16d01805fdf22b2021ab9b
MidoriKsai <minoginadara@gmail.com>
2025-12-26 23:55:15 +0500

    base

--- app/nats/nats_client.py
+++ app/nats/nats_client.py
@@ -1,7 +1,6 @@
 # app/nats/nats_client.py
 import asyncio
 from nats.aio.client import Client as NATS
-from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 import os
 from dotenv import load_dotenv
 
